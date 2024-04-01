ef4eaf11c553e9dc2f31419e93fd9c60d26959b0
Arseny Arnold <kulbakocnt@gmail.com>
2024-04-01 22:45:43 +0300

    added search with TextField

--- tarkov_price/lib/widgets/foreground.dart
+++ tarkov_price/lib/widgets/foreground.dart
@@ -3,8 +3,21 @@ import 'package:tarkov_price/api/requets.dart';
 
 
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final myController = TextEditingController();
+  List<dynamic> filteredNames = shortName;
+
+  @override
+  void dispose() {
+    // Очистка ресурсов контроллера при удалении виджета
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +28,8 @@ class MyApp extends StatelessWidget {
             begin: Alignment.topLeft,
             end: Alignment(0.8, 1),
             colors: <Color>[
-              Color.fromRGBO(163, 22, 33, 1),
-              Color.fromRGBO(78, 128, 152, 1),
-              Color.fromRGBO(252, 247, 248, 1),
+              Color.fromRGBO(28, 28, 28, 1),
+              Color.fromRGBO(18, 18, 18, 1),
             ],
           )
         ),
@@ -27,27 +39,46 @@ class MyApp extends StatelessWidget {
           Column(
             children: [
               const SizedBox(height: 25,),
-              const SizedBox(
-                    width: 250,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintStyle: TextStyle(color: Colors.white),
-                        hintText: "Item",
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: myController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintStyle: TextStyle(color: Colors.white),
+                            hintText: "Item",
+                          ),
+                          style: TextStyle(
+                            color: Colors.white
+                          ),
+                        ),
                       ),
-                      style: TextStyle(
-                        color: Colors.white
-                      ),
-                    ),
-                  ),
+                  IconButton(onPressed: () {
+                          int flag = 0;
+                          String result = myController.text;
+                          flag++;
+                          setState(() {
+
+                            if (flag == 1){
+                              filteredNames = shortName.where((name) => name == result).toList();
+                            }
+
+
+                          });
+                        }, icon: Icon(Icons.search))
+                ],
+              ),
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ListView(
-                    children: List.generate(shortName.length, (index) => Column(
+                    children: List.generate(filteredNames.length, (index) => Column(
                       children: [
                         const SizedBox(height: 25,),
-                        Button_item(name: shortName[index], price: low_price[index], update: updated[index],),  
+                        Button_item(name: filteredNames[index], price: low_price[index], update: updated[index],),  
                         const SizedBox(height: 25,),
                       ],
                     )),
@@ -77,38 +108,46 @@ class Button_item extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-    width: 350,
-    height: 100,
+    width: 450,
+    height: 150,
     child: ElevatedButton(
       onPressed: (){},
       style: ElevatedButton.styleFrom(
-        backgroundColor: const Color.fromRGBO(117, 117, 117, 1),
+        backgroundColor: const Color.fromRGBO(38, 38, 38, 1),
         foregroundColor: const Color.fromRGBO(117, 117, 117, 1),
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(32))
         )
       ), 
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: [
-          Row(
-            children: [
-              Text("$name", style: const TextStyle(color: Colors.white),),
-              const SizedBox(width: 30,),
-              const Text("Last Low Price: ",style: TextStyle(color: Colors.white),),
-              Text('$price', style: const TextStyle(color: Colors.white)),
-              const SizedBox(width: 30,),
-            ],
+          Center(
+            child: Row(
+              children: [
+                Text("$name", style: const TextStyle(color: Colors.white),),
+                const SizedBox(width: 30,),
+                const Text("Last Low Price: ",style: TextStyle(color: Colors.white),),
+                Text('$price', style: const TextStyle(color: Colors.white)),
+                const SizedBox(width: 30,),
+              ],
+            ),
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              
               const Text("Last update: ", style: TextStyle(color: Colors.white), ),
               Text("$update", style: const TextStyle(color: Colors.white),),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
-    ),
-                      );
+    );
   }
+
 }
+
+
